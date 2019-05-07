cf27f9649e28f624b4fe4f71718d22e5e5fb8a5d
roelcantu <roelhcantu@gmail.com>
2019-05-06 20:39:16 -0500

    Added isBlank method for null values.

--- FileUpload/src/main/java/controller/ImportCSV.java
+++ FileUpload/src/main/java/controller/ImportCSV.java
@@ -37,9 +37,6 @@ public class ImportCSV {
             String[] rowData = null;
             int i = 0;
             
-            readLines(file);
-
-
             while((rowData = reader.readNext()) != null)
             {
             	
@@ -77,23 +74,4 @@ public class ImportCSV {
         }
         return true;
     }
-	
-	public void readLines(String file) {
-        Scanner s = null;
-        try {
-            s = new Scanner(new File(file));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        
-        while (s.hasNextLine()) {
-            String[] line = s.nextLine().split(",");
-            for (String element : line) {
-                if (!isBlank(element))
-                    System.out.println(element);
-            }
-
-        }
-	}
-
 }
